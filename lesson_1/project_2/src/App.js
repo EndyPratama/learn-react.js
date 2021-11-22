4c43969bacd3b511b4b856f4504e5f6728aefa89
Endy Gigih Pratama <endypratama2999@gmail.com>
2021-11-22 13:28:25 +0700

    count

---
+++ lesson_1/project_2/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+  }
+
+  // Buat method handleClick
+  handleClick(){
+    this.setState({count : this.state.count + 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.count}
+        </h1>
+        {/* tambahkan event onClick di tag <button> */}
+        <button onClick = {()=>{this.handleClick()}}>+</button>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
